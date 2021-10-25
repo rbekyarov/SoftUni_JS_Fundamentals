6ae73df5449850a9030ed02528864a94739b25cf
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-10-25 15:59:02 +0300

    Arrays

--- 03.Arrays/6. Even and Odd Subtraction.js
+++ 03.Arrays/6. Even and Odd Subtraction.js
@@ -1,14 +1,15 @@
 function solve(arr){
     let sumEven = 0;
+    let sumOdd = 0
     for (let i = 0; i <arr.length ; i++) {
         let currentElement = Number(arr[i]);
         if (currentElement%2===0){
             sumEven +=currentElement;
+        }else {
+            sumOdd +=currentElement;
         }
     }
 
-    console.log(sumEven);
+    console.log(sumEven - sumOdd);
 }
-solve(['1','2','3','4','5','6']);
-solve(['3','5','7','9']);
-solve(['2','4','6','8','10']);
+solve([1,2,3,4,5,6]);

6b627e7452a6235b1350e40e5a00e574046ba1d6
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-10-27 11:10:51 +0300

    Functions in JS

---
+++ 04.Functions/4. Orders.js
@@ -0,0 +1,24 @@
+function solve(product,quantity){
+    let price = 0;
+    let totalPrice = 0;
+    switch (product){
+        case "coffee":
+            price = 1.50;
+            break;
+        case "water":
+            price = 1.00;
+            break;
+        case "coke":
+            price = 1.40;
+            break;
+        case "snacks":
+            price = 2.00;
+            break;
+    }
+    totalPrice = price*quantity;
+   // console.log(`${totalPrice.toFixed(2)}`);
+    return `${totalPrice.toFixed(2)}`;
+
+}
+solve("water",5);
+solve("coffee",2);
